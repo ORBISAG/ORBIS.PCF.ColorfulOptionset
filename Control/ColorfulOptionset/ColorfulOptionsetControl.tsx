32af093cdf066b3d49001cd6319ca7a1dbe02502
Diana Birkelbach <diana.birkelbach@orbis.de>
2020-01-31 14:50:38 +0100

    fixed reflect value changed outside

--- Control/ColorfulOptionset/ColorfulOptionsetControl.tsx
+++ Control/ColorfulOptionset/ColorfulOptionsetControl.tsx
@@ -14,26 +14,42 @@ import {ISelectableOption} from "office-ui-fabric-react/lib/SelectableOption";
   }*/
 
 initializeIcons();
+const styles: Partial<IDropdownStyles> = {
+  title: [{
+    borderColor: "transparent",                
+    outline: "none"            
+  }], 
+  caretDown :[{
+    color: "transparent"
+  }] 
+};
 
-const _renderOption =(option: ISelectableOption | undefined, className ?:string) : JSX.Element => {
-  return (
-    <div className={className}>
-        <Icon className="colorIcon" style={{color: option?.data?.color || "#ffffff", marginRight: "8px"}} iconName="CircleShapeSolid" aria-hidden="true" />          
-      <span>{option?.text || ""}</span>
-    </div>
-  );  
-}
-
-const _onRenderOption = (option: ISelectableOption | undefined): JSX.Element => {
-    return _renderOption(option, "item")
-  };
-
-const _onRenderTitle = (options: IDropdownOption[] | undefined): JSX.Element => {
-    const option = (options || [])[0];
-    return _renderOption(option, "option");
-        
-  };
-
+const myTheme = createTheme({
+  disableGlobalClassNames: true,
+  palette: {
+    themePrimary: '#a9a9a9',
+    themeLighterAlt: '#fcfcfc',
+    themeLighter: '#f1f1f1',
+    themeLight: '#e5e5e5',
+    themeTertiary: '#cbcbcb',
+    themeSecondary: '#b3b3b3',
+    themeDarkAlt: '#979797',
+    themeDark: '#808080',
+    themeDarker: '#5e5e5e',
+    neutralLighterAlt: '#faf9f8',
+    neutralLighter: '#f3f2f1',
+    neutralLight: '#edebe9',
+    neutralQuaternaryAlt: '#e1dfdd',
+    neutralQuaternary: '#d0d0d0',
+    neutralTertiaryAlt: '#c8c6c4',
+    neutralTertiary: '#a19f9d',
+    neutralSecondary:  '#605e5c',
+    neutralPrimaryAlt: '#3b3a39',
+    neutralPrimary: '#323130',
+    neutralDark: '#201f1e',
+    black: '#000000',
+    white: '#ffffff'                  
+  }});
 
 interface IColorfulOptionsetProperties {
     options: IDropdownOption[];
@@ -44,74 +60,48 @@ interface IColorfulOptionsetProperties {
 
 //export default class ColorfulOptionsetControl extends React.Component<IColorfulOptionsetProperties, {}> {            
 export const ColorfulOptionsetControl = ({options, selectedKey, onChange, isDisabled}:IColorfulOptionsetProperties): JSX.Element =>{
-    const [value, setValue] = React.useState(selectedKey);   
-
-    React.useEffect(() => {     
-        onChange(value);      
-    }, [value]);
-      
-    const _onSelectedChanged = (option?: IDropdownOption) => {
-        const newVal  = (option == null || option.key===-1) ? null : option.key as number;
-        setValue(newVal);            
+  console.log(selectedKey);
+            
+    const _onSelectedChanged = (option?: IDropdownOption) => {       
+      const val = (option?.key == null || option?.key===-1) ? null : option?.key as number;
+      onChange(val);      
     }
+
+    const _renderOption =(option: ISelectableOption | undefined, className ?:string) : JSX.Element => {
+      return (
+        <div className={className}>
+            <Icon className="colorIcon" style={{color: option?.data?.color || "#ffffff", marginRight: "8px"}} iconName="CircleShapeSolid" aria-hidden="true" />          
+          <span>{option?.text || ""}</span>
+        </div>
+      );  
+    }
+    
+    const _onRenderOption = (option: ISelectableOption | undefined): JSX.Element => {
+        return _renderOption(option, "item")
+      };
+    
+    const _onRenderTitle = (options: IDropdownOption[] | undefined): JSX.Element => {
+        const option = (options || [])[0];
+        return _renderOption(option, "option");
+            
+      };
    
       
-      const styles: Partial<IDropdownStyles> = {
-        title: [{
-          borderColor: "transparent",                
-          outline: "none"            
-        }], 
-        caretDown :[{
-          color: "transparent"
-        }] 
-      };
-     
-      const myTheme = createTheme({
-        disableGlobalClassNames: true,
-        palette: {
-          themePrimary: '#a9a9a9',
-          themeLighterAlt: '#fcfcfc',
-          themeLighter: '#f1f1f1',
-          themeLight: '#e5e5e5',
-          themeTertiary: '#cbcbcb',
-          themeSecondary: '#b3b3b3',
-          themeDarkAlt: '#979797',
-          themeDark: '#808080',
-          themeDarker: '#5e5e5e',
-          neutralLighterAlt: '#faf9f8',
-          neutralLighter: '#f3f2f1',
-          neutralLight: '#edebe9',
-          neutralQuaternaryAlt: '#e1dfdd',
-          neutralQuaternary: '#d0d0d0',
-          neutralTertiaryAlt: '#c8c6c4',
-          neutralTertiary: '#a19f9d',
-          neutralSecondary:  '#605e5c',
-          neutralPrimaryAlt: '#3b3a39',
-          neutralPrimary: '#323130',
-          neutralDark: '#201f1e',
-          black: '#000000',
-          white: '#ffffff'                  
-        }});
+      
      
     return (
-        <Dropdown
+        <Dropdown        
             placeHolder="---"
             options={options}
-            selectedKey={value || -1}  
+            //defaultValue={value || -1}
+            selectedKey={selectedKey || -1}  
             onRenderTitle = {_onRenderTitle}            
             onRenderOption = {_onRenderOption}            
-            onChanged={_onSelectedChanged}  
-           // onRenderCaretDown={_onRenderCaretDown}
-            //className="main" 
+            onChanged={_onSelectedChanged}                         
             disabled={isDisabled} 
-            className="ComboBox"
-           /* className={`ComboBoxMain ${className.className}`} 
-            onClick={() => dispatch({type: "CLICK"})} 
-            onMouseEnter={() => dispatch({type: "ENTER"})} 
-            onMouseLeave={() => dispatch({type:"LEAVE"})}                              */
-           styles={styles} 
-           theme = {myTheme}
-           
+            className="ComboBox"                        
+            styles={styles} 
+            theme = {myTheme}           
         />
     );
 
